7e1b8cb2c6773e4eb7f0a1348902bfad5b2d3952
kleverkarl <karlbayron18@gmail.com>
2020-08-17 15:37:33 +0800

    Force Destop View

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
             <img src={icon} alt='' className='fl pa2'/>
             <h1 className='pa3'> Cloudy Home</h1>
           </div>
-          <div className='tc bg-near-white bw2 mh7 br3 shadow-4'>
+          <div className='tc bg-near-white bw2 mh7 br3 shadow-4 mb4'>
             <CardList links={search} name='Search Engines' />
             <CardList links={social} name='Social Media' />
            </div>
-            <h1> Hello </h1>
+
         </div>
       )
   }
