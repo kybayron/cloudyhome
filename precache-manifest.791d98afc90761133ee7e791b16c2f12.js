3d8d12ef43fa8f3635623b1916bddbf205499d31
kleverkarl <karlbayron18@gmail.com>
2020-08-17 15:21:22 +0800

    Updates

---
+++ precache-manifest.791d98afc90761133ee7e791b16c2f12.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb873da1290aefb8b3be623b45316bf",
+    "url": "/cloudyhome/index.html"
+  },
+  {
+    "revision": "d452f3e72cb116652d4e",
+    "url": "/cloudyhome/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3637bea560f350d85fba",
+    "url": "/cloudyhome/static/css/main.c2a2f49a.chunk.css"
+  },
+  {
+    "revision": "d452f3e72cb116652d4e",
+    "url": "/cloudyhome/static/js/2.99111540.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cloudyhome/static/js/2.99111540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3637bea560f350d85fba",
+    "url": "/cloudyhome/static/js/main.9870eee5.chunk.js"
+  },
+  {
+    "revision": "a835a11a027db0f29aa6",
+    "url": "/cloudyhome/static/js/runtime-main.daac5875.js"
+  },
+  {
+    "revision": "22725e296b5e3f3600fcb093add00915",
+    "url": "/cloudyhome/static/media/bing_logo.22725e29.png"
+  },
+  {
+    "revision": "7603910d39d9b500122232e4f4ef5d60",
+    "url": "/cloudyhome/static/media/duckduckgo_logo.7603910d.png"
+  },
+  {
+    "revision": "493ccdf1a11a042a2de290ea6ff4a4e1",
+    "url": "/cloudyhome/static/media/ecosia_logo.493ccdf1.png"
+  },
+  {
+    "revision": "ff629c02385f2f3a199b7e0b3065d077",
+    "url": "/cloudyhome/static/media/google_logo.ff629c02.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/cloudyhome/static/media/instagram_logo.097b7749.png"
+  },
+  {
+    "revision": "cd606974b5ede2e602e0f038a3b5cf16",
+    "url": "/cloudyhome/static/media/tiktok_logo.cd606974.png"
+  },
+  {
+    "revision": "d28771926ba5cccc49cb87e01764f7f9",
+    "url": "/cloudyhome/static/media/twitter_logo.d2877192.png"
+  },
+  {
+    "revision": "a6ea4750da0229638bf31835dadef054",
+    "url": "/cloudyhome/static/media/youtube_logo.a6ea4750.png"
+  }
+]);
